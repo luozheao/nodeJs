199da7969a2fdecc60c62804d32b28e434e16d2b
luozheao <815149054@qq.com>
2018-05-10 01:45:37 +0800

    完成数据库查询功能

--- node/crawlerFile/sql.js
+++ node/crawlerFile/sql.js
@@ -3,6 +3,8 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'luozheao',
   password : 'luojie123',
+  // user     : 'root',
+  // password : '123456',
   database : 'forimage'
 });
 connection.connect();
